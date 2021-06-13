fa46b07ed69f227119078cc62a4b4ac6412bb6e9
Izabela <izabela.tarasin@gmail.com>
2021-06-14 00:09:45 +0200

    added upload file functionality

--- index.php
+++ index.php
@@ -8,5 +8,6 @@ $path = parse_url( $path, PHP_URL_PATH);
 Router::get('', 'DefaultController');
 Router::get('places', 'DefaultController');
 Router::post('login', 'SecurityController');
+Router::post('add_place','PlaceController');
 
 Router::run($path);
